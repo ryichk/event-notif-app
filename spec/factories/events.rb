5fbb5e659fa9e63107dc76349af0ab67a5c4873c
ryichk <ryichk@gmail.com>
2022-09-26 01:52:59 +0900

    :sparkles: UserとEventのFactoryBot定義を追加

---
+++ spec/factories/events.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :event do
+    owner
+    sequence(:name) { |i| "イベント名#{i}" }
+    sequence(:place) { |i| "place#{i}" }
+    sequence(:content) { |i| "content#{i}" }
+    start_at { rand(1..30).days.from_now }
+    end_at { start_at + rand(1..10).hours }
+  end
+end
