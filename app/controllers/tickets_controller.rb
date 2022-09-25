28465df75112749ba6d3d190a42ae3df3e76a534
ryichk <ryichk@gmail.com>
2022-09-25 19:16:37 +0900

    :sparkles: イベント参加キャンセル機能を追加

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -13,4 +13,10 @@ class TicketsController < ApplicationController
       redirect_to event, notice: "#{event.name}への参加表明が完了しました"
     end
   end
+
+  def destroy
+    ticket = current_user.tickets.find_by!(event_id: params[:event_id])
+    ticket.destroy!
+    redirect_to event_path(params[:event_id]), notice: 'イベントへの参加をキャンセルしました'
+  end
 end
