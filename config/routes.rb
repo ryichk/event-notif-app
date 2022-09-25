e38d4c3c38782e85b2877fe98ce6f07905a3816e
ryichk <ryichk@gmail.com>
2022-09-25 18:11:02 +0900

    :sparkles: イベント参加表明機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/auth/:provider/callback' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+  resources :events do
+    resources :tickets
+  end
 end
