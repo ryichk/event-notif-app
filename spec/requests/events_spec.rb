e3884cc188e05e9d13c07ca99092ecb218230306
Ichiki Ryo <tohodaiichiki@gmail.com>
2022-09-27 21:09:27 +0900

    :white_check_mark: イベント削除に関するリクエストスペックを追加

---
+++ spec/requests/events_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "Events", type: :request do
+  describe "DELETE /events/:id" do
+    it '自ら作成したイベントは削除できる' do
+      event_owner = FactoryBot.create(:user)
+      event = FactoryBot.create(:event, owner: event_owner)
+      sign_in_as event_owner
+
+      count = Event.count
+      delete event_path(event)
+      expect(Event.count).to eq count - 1
+    end
+
+    it '他のユーザが作成したイベントは削除できない' do
+      event_owner = FactoryBot.create(:user)
+      event = FactoryBot.create(:event, owner: event_owner)
+
+      sign_in_user = FactoryBot.create(:user)
+      sign_in_as sign_in_user
+
+      count = Event.count
+      assert_raises(ActiveRecord::RecordNotFound) do
+        delete event_path(event)
+      end
+      expect(Event.count).to eq count
+    end
+  end
+end
