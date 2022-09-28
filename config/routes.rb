1cfff4d23bd421112560d91810c62106d0d26687
Ichiki Ryo <tohodaiichiki@gmail.com>
2022-09-29 08:09:49 +0900

    fix: 利用するアクションのみに制限

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
-  resources :events do
-    resources :tickets
+  resources :events, only: %i[new create show edit update destroy] do
+    resources :tickets, only: %i[new create destroy]
   end
 
-  resource :retirements
+  resource :retirements, only: %i[new create]
 
   match '*path' => 'application#error404', via: :all
 end
