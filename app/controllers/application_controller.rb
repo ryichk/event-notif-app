d111fb4e2e0e19415fb4d7fcb71b93ef446e865b
Ichiki Ryo <tohodaiichiki@gmail.com>
2022-09-29 07:50:34 +0900

    :sparkles: コントローラレベルのエラーハンドリングを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate
   helper_method :logged_in?, :current_user
 
+  rescue_from Exception, with: :error500
+  rescue_from ActiveRecord::RecordNotFound,
+    ActionController::RoutingError, with: :error404
+
   private
 
   def logged_in?
@@ -16,5 +20,14 @@ class ApplicationController < ActionController::Base
   def authenticate
     return if logged_in?
     redirect_to root_path, alert: 'ログインしてください'
+  end
+
+  def error404(error)
+    render 'error404', status: 404, formats: [:html]
+  end
+
+  def error500(error)
+    logger.error [error, *error.backtrace].join('\n')
+    render 'error500', status: 500, formats: [:html]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   end
 
   resource :retirements
+
+  match '*path' => 'application#error404', via: :all
 end
